c12a7adb2ff6fc73839f415472f52cf9330762b0
1langit <langitlintangradjendra@mail.ugm.ac.id>
2023-09-05 22:52:26 +0700

    first commit

---
+++ resources/views/tugas.blade.php
@@ -0,0 +1,14 @@
+@extends('about')
+
+@section('hello')
+<h2>Hello world!</h2>
+<ol>
+    <li>satu</li>
+    <li>dua</li>
+    <li>empat</li>
+</ol>
+@endsection
+
+@section('goodbye')
+<h2>Goodbye world!</h2>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+$data = array("name" => "lala", "mail" => "lala@mail.com");
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () use($data) {
+    return view('about', $data);
+});
+
+Route::get('/tugas', function () use($data) {
+    return view('tugas', $data);
+});
